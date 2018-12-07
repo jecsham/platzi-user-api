e02314167ba24d8d0b306275fca6ef788fedbb60
Jecsham <jecsham27@gmail.com>
2018-12-07 17:31:48 -0500

    fixing headers

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,12 @@ const API_PATH_V1 = '/api/v1'
 const apiv1 = require('../controllers/api_v1')
 module.exports = (app) => {
     //V1
-    app.get(`${API_PATH_V1}/getUserSummary/:user`, async (req, res) =>{
+    app.get(`${API_PATH_V1}/getUserSummary/:user`, async (req, res) => {
         let user = req.params.user
-        res.header("Content-Type",'application/json');
-        res.send(JSON.stringify(await apiv1.getUserSummary(user), null, 4));
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        res.header("Content-Type", 'application/json')
+        res.send(JSON.stringify(await apiv1.getUserSummary(user), null, 4))
     })
 
 }
