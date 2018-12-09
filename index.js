75286a573977967f81c6e4161af860d9029fa7df
Jecsham <jecsham27@gmail.com>
2018-12-08 21:20:43 -0500

    cache added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const app = express()
-require('dotenv').config()
+const apicache = require("apicache")
 const rateLimit = require("express-rate-limit")
+require('dotenv').config()
+const app = express()
 app.enable("trust proxy")
 app.use(express.static('public'))
 const apiLimiter = rateLimit({
@@ -13,11 +14,17 @@ const apiLimiter = rateLimit({
             code: 429
         }
         let userData = {}
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
         res.header("Content-Type", 'application/json')
-        res.send(JSON.stringify({ status, userData }, null, 4))
+        res.send(JSON.stringify({ status, userData }, null, 4)).status()
     }
 })
+let cache = apicache.middleware
+const onlyStatus200 = (req, res) => res.statusCode === 200
+const cacheSuccesses = cache('10 minutes', onlyStatus200)
 app.use("/api/", apiLimiter)
+app.use(cacheSuccesses)
 require('./routes')(app)
 app.listen(process.env.PORT, () => {
     console.info("Server started")
