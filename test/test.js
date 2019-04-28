7bf3a513de9fabe4d8501ca594b4d6bb0c059423
Jecsham <jecsham27@gmail.com>
2019-04-28 13:18:05 -0500

    linted ./test/test.js

--- test/test.js
+++ test/test.js
@@ -1,29 +1,29 @@
-const assert = require('assert')
-const apiv1 = require('../lib/api_v1')
-describe('API V1', function () {
-    describe('#scrapePlatziUser()', function () {
-        it('should return "Jecsham Castillo" with the param "@jecsham"', async function () {
-            let data = await apiv1.scrapePlatziUser('@jecsham')
-            assert.equal(data.userData.name, "Jecsham Castillo")
+const assert = require("assert");
+const apiv1 = require("../lib/api_v1");
+describe("API V1", function () {
+    describe("#scrapePlatziUser()", function () {
+        it("should return \"Jecsham Castillo\" with the param \"@jecsham\"", async function () {
+            let data = await apiv1.scrapePlatziUser("@jecsham");
+            assert.equal(data.userData.name, "Jecsham Castillo");
         });
     });
-    describe('#scrapePlatziUser() - private profile (403)', function () {
-        it('should return status code 403 with the param "@rleonel"', async function () {
-            let data = await apiv1.scrapePlatziUser('@rleonel')
-            assert.equal(data.status.code, 403)
+    describe("#scrapePlatziUser() - private profile (403)", function () {
+        it("should return status code 403 with the param \"@rleonel\"", async function () {
+            let data = await apiv1.scrapePlatziUser("@rleonel");
+            assert.equal(data.status.code, 403);
         });
     });
-    describe('#scrapePlatziUser() - error (404)', function () {
-        it('should return status code 404 with the param "randomroute"', async function () {
-            let data = await apiv1.scrapePlatziUser('randomroute')
-            assert.equal(data.status.code, 404)
+    describe("#scrapePlatziUser() - error (404)", function () {
+        it("should return status code 404 with the param \"randomroute\"", async function () {
+            let data = await apiv1.scrapePlatziUser("randomroute");
+            assert.equal(data.status.code, 404);
         });
     });
-    describe('#insertOrUpdateUser()', function () {
-        it('should return true with the param apiv1.scrapePlatziUser("@jecsham")', async function () {
-            let data = await apiv1.scrapePlatziUser('@jecsham')
-            let result = await apiv1.insertOrUpdateUser(data.userData)
-            assert.equal(result, true)
+    describe("#insertOrUpdateUser()", function () {
+        it("should return true with the param apiv1.scrapePlatziUser(\"@jecsham\")", async function () {
+            let data = await apiv1.scrapePlatziUser("@jecsham");
+            let result = await apiv1.insertOrUpdateUser(data.userData);
+            assert.equal(result, true);
         });
     });
 });
